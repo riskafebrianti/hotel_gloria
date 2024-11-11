0ad3f0608f2da1e947c7c971fe7c07690f56886c
unknown <riskaafebri@gmail.com>
2024-11-11 15:04:41 +0800

    purchase

--- addon_hotel/models/purchase.py
+++ addon_hotel/models/purchase.py
@@ -9,5 +9,20 @@ class Purchase(models.Model):
         string='Deskripsi',
     )
     
+    @api.onchange('order_line')
+    def get_name(self):
+        isiqty = []
+        for a in self.order_line:
+            qty = int(a.product_qty)
+            uom = a.product_uom.name
+            name = a.name
+            subtotal = '{0:,.0f}'.format(a.price_subtotal)
+            isiqty.append(f"Pembelian {qty} {uom} {name} @Rp.{subtotal} ")
+            self.deskripsi = ', '.join(map(str, isiqty))+ ", ".join([f"({vendor.partner_id.name})" for vendor in a.order_id])
+            # subtotal = int(a.price_subtotal)
+            # vendor = a.order_id.partner_id.name
+            # combined_names = ', '.join(map(str, isiqty))
+            # combined_names =  isiqty
+    
 
     
