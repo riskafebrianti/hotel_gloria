456cabee2aed4b6b0682e317dde714d0ddf3b41b
unknown <riskaafebri@gmail.com>
2024-11-18 22:55:30 +0800

    invsible button when user not admin and bu rahma rev 2

--- addon_hotel/__manifest__.py
+++ addon_hotel/__manifest__.py
@@ -27,7 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/room.xml',
+        # 'views/room.xml',
         'views/res_partner.xml',
         'views/maintenance_req.xml',
         'views/account_payment.xml',
